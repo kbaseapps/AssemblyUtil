7ff88f93bfd02e034c1a87ed6c86bda86f4c6dfb
Jason Kirk Baumohl <jkbaumohl@14.151.3.128.in-addr.dhcp.lbl.gov>
2017-11-15 15:04:06 -0800

    Changed Unit test accordingly for no contigs

--- test/AssemblyUtil_server_test.py
+++ test/AssemblyUtil_server_test.py
@@ -231,15 +231,11 @@ class AssemblyUtilTest(unittest.TestCase):
         fasta_path = os.path.join(tmp_dir, file_name)
         print('attempting upload')
         ws_obj_name = 'FilteredAssembly'
-        result = assemblyUtil.save_assembly_from_fasta(self.getContext(),
+        with self.assertRaises(ValueError) as context:
+            assemblyUtil.save_assembly_from_fasta(self.getContext(),
                                                        {'file': {'path': fasta_path},
                                                         'workspace_name': self.getWsName(),
                                                         'assembly_name': ws_obj_name,
                                                         'min_contig_length': 500
                                                         })
-
-        dfu = DataFileUtil(os.environ['SDK_CALLBACK_URL'])
-        assembly = dfu.get_objects({'object_refs': [result[0]]})['data'][0]['data']
-        self.assertEqual(assembly['dna_size'], 0)
-        self.assertEqual(assembly['gc_content'], None)
-        self.assertEqual(assembly['num_contigs'], 0)
+        self.assertEqual('There are no contigs to save, thus there is no valid assembly.', str(context.exception.message))
