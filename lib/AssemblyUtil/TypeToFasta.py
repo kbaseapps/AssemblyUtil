8b150124f98c4eddbdb728ad9178739406d6b100
Cheyenne Nelson <cheyenne.nelson@berkeley.edu>
2019-06-25 10:11:44 -0700

    correctly implemented object subsetting for genome objects in genome_obj_to_fasta function.

--- lib/AssemblyUtil/TypeToFasta.py
+++ lib/AssemblyUtil/TypeToFasta.py
@@ -27,7 +27,7 @@ class TypeToFasta:
         upas = []
 
         if 'KBaseSets.GenomeSet' in obj_type:
-            obj_data = self.ws.get_objects2({'objects': [{"ref": ref, 'included': 'data'}]})[0]
+            obj_data = self.ws.get_objects2({'objects': [{"ref": ref}]})['data'][0]
             upas = [gsi['ref'] for gsi in obj_data['data']['items']]
         elif 'KBaseSearch.GenomeSet' in obj_type:
             obj_data = self.ws.get_objects2({'objects': [{"ref": ref}]})['data'][0]
@@ -37,12 +37,15 @@ class TypeToFasta:
 
         if upas:
             for genome_upa in upas:
-                genome_data = self.ws.get_objects2({'objects': [{"ref": genome_upa}]})['data'][0]['data']
-                assembly_upa = genome_upa + ';' + str(genome_data.get('contigset_ref') \
-                                                      or genome_data.get('assembly_ref'))
+                genome_data = self.ws.get_objects2({'objects': [{"ref": genome_upa, 'included' : ['/assembly_ref/']}]}) \
+                                ['data'][0]['data']
+                if genome_data:
+                    assembly_upa = genome_upa + ';' + str(genome_data.get('assembly_ref'))
+                    faf = atf.assembly_as_fasta({'ref': assembly_upa})
+                    fasta_dict[assembly_upa] = faf['path']
 
-                faf = atf.assembly_as_fasta({'ref': assembly_upa})
-                fasta_dict[assembly_upa] = faf['path']
+                else:
+                    raise TypeError("KBase object type %s does not contain an assembly reference." % obj_type)
 
         return fasta_dict
 
@@ -68,14 +71,20 @@ class TypeToFasta:
     def metagenome_obj_to_fasta(self, ref, obj_type, fasta_dict):
 
         if 'KBaseMetagenomes.BinnedContigs' in obj_type:
+            fasta_paths = []
             try:
                 bin_file_dir = self.mgu.binned_contigs_to_file({'input_ref': ref, 'save_to_shock': 0}) \
                     ['bin_file_directory']
                 for (dirpath, dirnames, filenames) in os.walk(bin_file_dir):
+
                     for fasta_file in filenames:
+                        # Copy file to scratch
                         fasta_path = os.path.join(self.scratch, fasta_file)
+                        fasta_path = os.path.splitext(fasta_path)[0] + ".fa"
                         copyfile(os.path.join(bin_file_dir, fasta_file), fasta_path)
-                        fasta_dict[ref] = fasta_path
+                        fasta_paths.append(fasta_path)
+                fasta_dict[ref] = fasta_paths
+
             except _MGUError as mgue:
                 self.log('Logging exception loading binned contigs to file.')
                 self.log(str(mgue))
@@ -84,7 +93,6 @@ class TypeToFasta:
         return fasta_dict
 
     def type_to_fasta(self, ref_lst):
-
 
         # Initiate objects
         fasta_dict = {}
